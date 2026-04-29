818b6f635c5023b7a68e55170e2d90ef99d29ea1
GengisThatBruh <kandavis63@gmail.com>
2026-04-28 23:17:13 -0400

    printing formatted strings

--- webscraper.py
+++ webscraper.py
@@ -18,6 +18,12 @@ def get_price(card): # parses through html to print out price
         return card.find("span", itemprop="price").text.strip()
     except AttributeError:
         return "N/A"
+    
+def display(name, price, rating): # print formatted strings from previous returns
+    print(f"Laptop: {name}")
+    print(f"Price: {price}")
+    print(f"Rating: {rating}")
+    print("-" * 30) 
 
 
 
